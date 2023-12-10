c2a4099f4df729967444d649b68c6b456b156179
Janani Hemamali <66689414+janaweerakkody@users.noreply.github.com>
2023-12-11 02:08:46 +0530

    Initial Commit

---
+++ src/main/java/developerIQ/useractivitymonitorservice/UserActivityMonitorServiceApplication.java
@@ -0,0 +1,13 @@
+package developerIQ.useractivitymonitorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityMonitorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityMonitorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/developerIQ/useractivitymonitorservice/UserActivityMonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package developerIQ.useractivitymonitorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityMonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
