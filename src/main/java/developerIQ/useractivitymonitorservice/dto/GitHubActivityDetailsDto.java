3fac773a9bd9d3a861faf70c02bcfb635b806280
Janani Hemamali <66689414+janaweerakkody@users.noreply.github.com>
2023-12-26 14:59:08 +0530

    Change to get Issue Count

--- src/main/java/developerIQ/useractivitymonitorservice/dto/GitHubActivityDetailsDto.java
+++ src/main/java/developerIQ/useractivitymonitorservice/dto/GitHubActivityDetailsDto.java
@@ -1,6 +1,7 @@
 package developerIQ.useractivitymonitorservice.dto;
 
 import developerIQ.useractivitymonitorservice.model.GitHubActivity;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
